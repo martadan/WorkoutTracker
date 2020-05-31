9b8d9e599f2822c1e748372ccf3d2dbc2356aa60
Danil <dsm013@gmail.com>
2020-05-31 12:56:55 -0500

    Reordered models.py file

--- models.py
+++ models.py
@@ -16,31 +16,6 @@ def setup_db(app, database_path=database_path):
     db.create_all()
 
 
-# Will possibly expand to this in the future, but not yet
-# class Event(db.Model):
-#     __tablename__ = 'Events'
-#
-#     id = Column(Integer(), primary_key=True)
-#     date = Column(Date(), nullable=False)
-#     category = Column(String(), nullable=False)
-#     subcategory = Column(String())
-#     time = Column(Time(), nullable=False)
-#
-#     def __init__(self, date, category, subcategory, time):
-#         self.date = date
-#         self.category = category
-#         self.subcategory = subcategory
-#         self.time = time
-#
-#     def format(self):
-#         return {
-#             'id': self.id,
-#             'date': self.name,
-#             'category': self.category,
-#             'subcategory': self.subcategory,
-#             'time': self.time
-#         }
-
 class Workout(db.Model):
     __tablename__ = 'Workouts'
 
@@ -91,3 +66,29 @@ class Exercise(db.Model):
             'target': self.target,
             'link': self.link
         }
+
+
+# Will possibly expand to this in the future, but not yet
+# class Event(db.Model):
+#     __tablename__ = 'Events'
+#
+#     id = Column(Integer(), primary_key=True)
+#     date = Column(Date(), nullable=False)
+#     category = Column(String(), nullable=False)
+#     subcategory = Column(String())
+#     time = Column(Time(), nullable=False)
+#
+#     def __init__(self, date, category, subcategory, time):
+#         self.date = date
+#         self.category = category
+#         self.subcategory = subcategory
+#         self.time = time
+#
+#     def format(self):
+#         return {
+#             'id': self.id,
+#             'date': self.name,
+#             'category': self.category,
+#             'subcategory': self.subcategory,
+#             'time': self.time
+#         }
