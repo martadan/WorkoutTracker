747c6ef3a0fde8e610dbf3cb8ebba14b6a0c0b62
Danil <dsm013@gmail.com>
2020-05-31 21:33:03 -0500

    Tried adding data to unittests. Couldn't get sqlalchemy to drop the tables at the end of each test  (the same values would try to be added to the database every test, which causes it to crash on unique constraints) Spent way too long trying to fix it, will come back to it later  (left portions in the code, but commented out)

--- test_app.py
+++ test_app.py
@@ -12,22 +12,23 @@ class WorkoutTestCase(unittest.TestCase):
     def setUp(self):
         """
         Define test variables and initialize app
+        Has to be in this awkward camelCase to overload method in TestCase
         """
         self.app = create_app()
         self.client = self.app.test_client
 
         self.database_path = os.environ['HEROKU_POSTGRESQL_AQUA_URL']
-        # TODO look into testing.postgresql
-        #  creates a temporary postgresql instance to test on instead of requiring a permanent database
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
+
             # create all tables
             self.db.create_all()
-            # populate tables with test values
+
+            # TODO populate tables with test values - this worked, but...
             # e1 = Exercise(
             #     name='kb swing',
             #     equipment='kettlebell',
@@ -49,8 +50,8 @@ class WorkoutTestCase(unittest.TestCase):
             # )
             # w.insert()
             # m = WorkoutExercise(
-            #     workout_id=1,
-            #     exercise_id=1,
+            #     workout_id=1,#e1.id,
+            #     exercise_id=1,#w.id,
             #     sets=3,
             #     reps=8,
             #     weight=4
@@ -60,14 +61,42 @@ class WorkoutTestCase(unittest.TestCase):
     def tearDown(self):
         """
         Executed after each test
+        Has to be in this awkward camelCase to overload method in TestCase
         """
         pass
+        # TODO drop all tables so they're recreated in the setUp method - spent hours on this, still not working
+        # with self.app.app_context():
+        #     self.db.drop_all()
+        #     # self.db.get_engine(self.app).dispose()
+
+        # # delete all WorkoutExercise mappings
+        # maps = WorkoutExercise.query.all()
+        # for m in maps:
+        #     m.delete()
+        #
+        # # delete all Exercises
+        # exercises = Exercise.query.all()
+        # for e in exercises:
+        #     e.delete()
+        #
+        # # delete all Workouts
+        # workouts = Workout.query.all()
+        # for w in workouts:
+        #     w.delete()
 
     def test_get_workouts(self):
         response = self.client().get('/workouts')
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['success'], True)
+        # self.assertEqual(len(data['workouts']), 1)
+
+    # def test_get_exercises(self):
+    #     response = self.client().get('/exercises')
+    #     data = json.loads(response.data)
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     # self.assertEqual(len(data['exercises']), 2)
 
 
 # Make the tests conveniently executable
